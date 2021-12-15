ffe3ee344566d5071934ad68a18a887b6c64440e
natto-natto <62298731+natto-natto@users.noreply.github.com>
2021-12-15 23:50:11 +0900

    Update Multiple_regression_model.py

--- Multiple_regression_model.py
+++ Multiple_regression_model.py
@@ -8,6 +8,7 @@ step3:目的関数を決める
 step4:最適化手法を選択する
 step5:モデルを訓練する
 """
+
 """
 step1:データセットの準備
 データセットの準備では訓練を行っていくデータをセットすることを行う
@@ -16,5 +17,18 @@ step1:データセットの準備
 1,scikit-learnに同梱されていてすぐにインポート出来るサイズの小さなデータ(Toy Datasets)
 2,ダウンロードすることで使えるサイズの大きいデータ(Real World Datasets)
 である。
+今回はToy Datasetsの「load_diabetes()」を使用する。
+(load_boston()で勉強したため別のデータセットを使用する)
+さて、実際にコードを書いていく
+load_diabetes()のインポートは次の文で可能
+"""
+
+from sklearn.datasets import load_diabetes
 
 """
+また、今回はpandasをインポートしてpandasのDataFrameを利用します。
+そうすることによってデータの可視化がされて見やすいです。
+以下の文はその他必要なライブラリを呼び出している
+"""
+
+
